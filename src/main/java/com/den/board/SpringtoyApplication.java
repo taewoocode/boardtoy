645eb847b17f67a9ecde1ab06258573140e6da57
taewoocode <qkrxodn6035@gmail.com>
2024-03-20 15:52:48 +0900

    첫번째 커밋

---
+++ src/main/java/com/den/board/SpringtoyApplication.java
@@ -0,0 +1,13 @@
+package com.den.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SpringtoyApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/den/board/SpringtoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.den.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
