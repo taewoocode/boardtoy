a8400c02359d06aa0e2191cef66819602b2e3e31
taewoocode <qkrxodn6035@gmail.com>
2024-03-21 14:32:47 +0900

    entity구현

--- src/main/java/com/den/board/controller/BoardController.java
+++ src/main/java/com/den/board/controller/BoardController.java
@@ -2,15 +2,25 @@ package com.den.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
+    @GetMapping("/board/write") //localhost8090에 boardwrite
+    public String boardWriteForm() {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
+        return "boardwrite";
 
-        return "Hello World";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(String title, String content) {
+
+        System.out.println( "제목 : " + title );
+        System.out.println( "내용 : " + content );
+
+        return "";
+
     }
 }
---
+++ src/main/java/com/den/board/entity/Board.java
@@ -0,0 +1,17 @@
+package com.den.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+}
