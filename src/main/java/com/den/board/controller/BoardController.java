8bc092a8614fb11851dc1db15e9609993babf7a8
taewoocode <qkrxodn6035@gmail.com>
2024-03-21 20:36:08 +0900

    end commit

--- src/main/java/com/den/board/controller/BoardController.java
+++ src/main/java/com/den/board/controller/BoardController.java
@@ -66,7 +66,7 @@ public class BoardController {
         return "boardlist";
     }
 
-    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    @GetMapping("/board/view") // localhost:8090/board/view?id=1
     public String boardView(Model model, Integer id) {
 
         model.addAttribute("board", boardService.boardView(id));
