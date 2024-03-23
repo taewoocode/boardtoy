9a8eda63e14b17575053b671b9e79eb4178d41d2
taewoocode <qkrxodn6035@gmail.com>
2024-03-23 15:44:59 +0900

    서비스 수정

--- src/main/java/com/den/board/service/BoardService.java
+++ src/main/java/com/den/board/service/BoardService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -54,4 +53,5 @@ public class BoardService {
 
         return boardRepository.findById(id).get();
     }
+
 }
